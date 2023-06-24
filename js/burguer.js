3bee063d301ab49f7b6482664a9b0f55a9aa016d
Hugo <maxconm190@gmail.com>
2023-06-24 15:11:52 -0600

    experiencia

--- js/burguer.js
+++ js/burguer.js
@@ -1,9 +1,15 @@
-
 function toggleMenu() {
     let menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
 
+// Agrega un listener a todos los elementos <a> del menú
+let menuLinks = document.querySelectorAll('.menu li a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', toggleMenu);
+});
+
+// Mantén el resto del código existente
 let burgerMenu = document.querySelector('.burger-menu');
 burgerMenu.addEventListener('click', toggleMenu);
 let exit = document.querySelector('.exit');
